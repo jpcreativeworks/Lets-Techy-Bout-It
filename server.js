0d56158e9fdb9598e38a827da00efa8db46b1409
jpcreativeworks <jp.graphics.011@gmail.com>
2022-05-19 09:25:34 -0700

    changes to serverjs

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const routes = require('./contollers')
 
 //middleware 
 const app = express();
-const PORT = process.envPORT || 8080
+const PORT = process.env.PORT || 8080
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(routes)
+app.use(routes);
+
+sql.sync({ force: false}).then(() => {
+    app.listen(PORT, () => console.log(`Now listening to PORT ${env.PORT}`));
+  });
